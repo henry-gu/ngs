74a4eaa97bbb4be2aa8bf85f61a47a5aca90b24f
henry-gu <henryguhb@outlook.com>
2023-07-15 17:40:19 +0800

    npm code

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,8 @@
-console.log(arguments);
+// function(exports, module, require, __filename, __dirname){
+
+//console.log(arguments);
+exports.a = 40; // exports is an argument of the wrapping funciton
+module.exports.b = 37;
+
+//return module.exports; //return this by default everytime
+// } (module.exports,)
