9e8540a79264ac1d3b94c95d1c4f56805b043242
henry-gu <henryguhb@outlook.com>
2023-07-15 15:06:58 +0800

    modern js sample code updated

--- 2-modern-js/7-objects.js
+++ 2-modern-js/7-objects.js
@@ -1,6 +1,12 @@
+const mystery="answer";
+
 const obj = {
   p1: 10,
   p2: 20,
   f1() {},
   f2: () => {},
+  [mystery]: 40, //dynamic attribute
 };
+
+console.log(obj.mystery);
+console.log(obj.answer);
