41475c05a5e6e311f067f45267967c5d6c4618dd
henry-gu <henryguhb@outlook.com>
2023-07-15 10:06:52 +0800

    1-getting-started/2-timer completed

--- 1-getting-started/2-timers/3-setTimeout-with-function-and-arguments.js
+++ 1-getting-started/2-timers/3-setTimeout-with-function-and-arguments.js
@@ -1,5 +1,5 @@
-const rocks = who => {
-  console.log(who + ' rocks');
+const rocks = (firstName, lastName) => {
+  console.log(`${firstName} ${lastName} rocks`);
 };
 
-setTimeout(rocks, 2 * 1000, 'Pluralsight');
+setTimeout(rocks, 2 * 1000, 'Node', 'Getting Started');
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let count=0;
+const intervalId=setInterval(()=>{
+        count++;
+        console.log("Node Getting Startted..."+ count);
+        if (count==5){
+            console.log("Done!");
+            clearInterval(intervalId);
+        }
+    },
+    2000
+);
